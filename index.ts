947aa30ca81c2547e6af8a3321eaed7138a554a8
yangfriendship <yangfriendship@naver.com>
2023-09-22 22:50:25 +0900

    refactor: 3.2.1 메서드 추출

--- index.ts
+++ index.ts
@@ -112,13 +112,7 @@ function update() {
   }
 }
 
-function draw() {
-  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
-  let g = canvas.getContext("2d");
-
-  g.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw map
+function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
       if (map[y][x] === Tile.FLUX)
@@ -138,10 +132,21 @@ function draw() {
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
+}
 
-  // Draw player
+function drawPlayer(g: CanvasRenderingContext2D) {
   g.fillStyle = "#ff0000";
   g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
+
+function draw() {
+  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
+  let g = canvas.getContext("2d");
+
+  g.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawMap(g);
+  drawPlayer(g);
 }
 
 function gameLoop() {
