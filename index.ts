25a1a76d2a6bae2d2379b6f6933de2706e863f68
yangfriendship <yangfriendship@naver.com>
2023-09-22 22:48:23 +0900

    refactor: 3.3.1 호출 또는 전달, 한 가지만 할 것

--- index.ts
+++ index.ts
@@ -139,11 +139,16 @@ function drawPlayer(g: CanvasRenderingContext2D) {
   g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
-function draw() {
+function createGraphic() {
   let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
   let g = canvas.getContext("2d");
 
   g.clearRect(0, 0, canvas.width, canvas.height);
+  return g;
+}
+
+function draw() {
+  let g = createGraphic();
 
   drawMap(g);
   drawPlayer(g);
